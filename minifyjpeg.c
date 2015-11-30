97dc26f8a980f5319d976e5abb4bb228311d22b2
Stewart Boyd <stewartboyd1988@gmail.com>
2015-11-30 00:24:28 -0500

    fixed memory leak

--- minifyjpeg.c
+++ minifyjpeg.c
@@ -4,15 +4,14 @@
 /* Implement the needed server-side functions here */
 minify_out * minify_proc_1_svc(minify_out in, struct svc_req *req)
 {
-	minify_out *out = (minify_out *)malloc(sizeof(minify_out));
+	static minify_out out;
 	size_t src_len;
 	magickminify_init();
-	out->src.src_val = magickminify(in.src.src_val, in.src.src_len, &src_len);
-	out->src.src_len = src_len;
-	printf("out->src.src_len = ", out->src.src_len);
-	//out->src_len = in.src_len;
+	out.src.src_val = magickminify(in.src.src_val, in.src.src_len, &src_len);
+	out.src.src_len = src_len;
+	printf("out->src.src_len = ", out.src.src_len);
 	magickminify_cleanup();
-	return out;
+	return &out;
 }
 
 
